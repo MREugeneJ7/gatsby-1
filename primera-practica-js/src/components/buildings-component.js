afc382e7eacfdef1cf93e2687aeb0b0122a09ccc
MREugeneJ7 <32437774+MREugeneJ7@users.noreply.github.com>
2024-01-12 19:03:55 +0000

    Avances componente

--- primera-practica-js/src/components/buildings-component.js
+++ primera-practica-js/src/components/buildings-component.js
@@ -1,11 +1,31 @@
 import * as React from "react"
 
-const BuildingComponent = () => (
-    <div>
+let edificios = [{
+    nombre: "Mi casa",
+    imagen: "https://i.blogs.es/c68014/casa-3d/840_560.jpeg",
+    antecedentes: "Antes vivia en una madriguera",
+    arquitectura: "rocambolesca",
+    epoca: "espacial",
+    coordenadas: {
+        lat: "aqui",
+        long: "aca"
+    }
+}]
+
+const BuildingComponent = () => {
+    const [index, setIndex] = React.useState(0);
+
+    return (
         <div>
-            <h2>Edificios</h2>
+            <div>
+                <h2>{edificios[index].nombre}</h2>
+                <img src={edificios[index].imagen} />
+                <p>Antecedentes: {edificios[index].antecedentes}</p>
+                <p> Es una construcción con arquitectura {edificios[index].arquitectura} construido en el periodo {edificios[index].epoca}</p>
+                <p>Esta situado en las coordenadas {edificios[index].coordenadas.lat}, {edificios[index].coordenadas.long}</p>
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
 export default BuildingComponent
